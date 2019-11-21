eed590f9f9ad580b9745b5524760fe622f34f5ad
Tyrik Gibbs <tyrikgibbs@outlook.com>
2019-11-21 01:44:13 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 def parrot(phrase = "Squawk!")
-  puts parrot = "#{phrase}"
+  parrot = "#{phrase}"
+  puts parrot
   return phrase
 end
